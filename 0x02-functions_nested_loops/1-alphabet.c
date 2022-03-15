373006af2751651f03d4c57a566a0849bd490aac
Chuypri <mwanaidmwinyi95@gmail.com>
2022-03-15 22:30:28 +0300

    My first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,1 @@
-#include "holberton.h"
 
-/**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line
- */
-void print_alphabet(void)
-{
-	char ch;
-
-	ch = 'a';
-	
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		:h++;
-	}
-	_putchar('\n');
-}
